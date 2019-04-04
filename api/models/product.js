af7b35517c1a0f95b3668b2ed01800ad4c3e6e9f
Alex Scheaua <alex.scheaua22@gmail.com>
2019-04-04 14:58:37 +0300

    v 1.0.0
    Admin object model sync

--- api/models/product.js
+++ api/models/product.js
@@ -5,15 +5,15 @@ const productSchema = mongoose.Schema({
   name: String,
   description: String,
   icon: String,
-  imgs: {
-    0: String,
-    1: String,
-    2: String,
-    3: String,
-    4: String,
-    5: String,
-    6: String
-  },
+  imgs: [
+     String,
+     String,
+     String,
+     String,
+     String,
+     String,
+     String
+  ],
   price: Number,
   discount: Number,
   discountedPrice: Number,
